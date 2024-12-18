ba741eee1df5ea5f4501440d3a9d6609113e9ed1
Andrei Konovalov <a.konovaloff13@yandex.com>
2024-12-18 08:11:44 +0400

    Added 4 more tests without descriptions

--- tests.py
+++ tests.py
@@ -20,7 +20,7 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_genre имеет длину 2
-        assert len(collector.books_genre.keys()) == 2
+        assert len(collector.get_books_genre().keys()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
@@ -47,3 +47,78 @@ class TestBooksCollector:
             collector.add_new_book(book)
             # проверяем, что книга только одна
             assert len(collector.books_genre.keys()) == 1
+
+    def test_get_book_genre_add_two_books_no_genre(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+
+        # добавляем две книги
+        book_1 = 'Book_1'
+        book_2 = 'Book_2'
+        collector.add_new_book(book_1)
+        collector.add_new_book(book_2)
+
+        # проверяем, что у книг нет жанра
+        assert collector.get_book_genre(book_1) == collector.get_book_genre(book_2) == ""
+
+    def test_set_book_genre_set_correct_genre_correct_genre(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+
+        # добавляем книгу
+        book = 'Book'
+        collector.add_new_book(book)
+        for genre in collector.genre:
+            collector.set_book_genre(book, genre)
+            assert collector.get_book_genre(book) == genre
+
+    def test_set_book_genre_set_incorrect_genre_no_genre(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+
+        # добавляем книгу
+        book = 'Book'
+        collector.add_new_book(book)
+        for genre in ["", "Ужосы", 0, 1]:
+            collector.set_book_genre(book, genre)
+            assert collector.get_book_genre(book) == ""
+
+    def test_get_books_with_specific_genre_assign_three_genres_return_two_books(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+
+        # выбираем 2 жанра
+        genre_1, genre_2 = collector.genre[0], collector.genre[1]
+
+        # создаем 3 названия
+        book_1 = f'1_{genre_1}'
+        book_2 = f'2_{genre_2}'
+        book_3 = f'3_{genre_2}'
+
+        # Добавляем все три книги, задаём жанры
+        collector.add_new_book(book_1)
+        collector.set_book_genre(book_1, genre_1)
+
+        collector.add_new_book(book_2)
+        collector.set_book_genre(book_2, genre_2)
+
+        collector.add_new_book(book_3)
+        collector.set_book_genre(book_3, genre_2)
+
+        # проверяем, что метод вернёт только книги 2 и 3
+        assert collector.get_books_with_specific_genre(genre_2) == [book_2, book_3]
+
+        # def test_get_books_with_specific_genre_assign_invalid_genres_no_genres(self):
+        #     # создаем экземпляр (объект) класса BooksCollector
+        #     collector = BooksCollector()
+        #     # создаем словарь с валидными именами книг и не валидными жанрами
+        #     books_genres = {"1": "",
+        #                     "2": "random_жанр_name",
+        #                     "3": 0}
+        #     for book, genre in books_genres.items():
+        #         if genre not in collector.genre:
+        #             collector.add_new_book(book)
+        #             collector.set_book_genre(book, genre)
+        #             print(collector.books_genre)
+        #             assert collector.books_genre[book] == ""
+
